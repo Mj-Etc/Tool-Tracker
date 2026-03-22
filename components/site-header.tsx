1cd524fda75845cda17dd2aab8403781de1a1ae6
Aezakmi <menardjayetcobanez@gmail.com>
2026-03-22 10:55:45 +0800

    Minor changes

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,6 +4,7 @@ import useSWR from "swr";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { ModeToggle } from "./ui/mode-toggle";
 import { fetcher } from "@/lib/fetcher";
+import Link from "next/link";
 
 type Stats = {
   outOfStock: number;
@@ -23,22 +24,24 @@ export function SiteHeader() {
         <SidebarTrigger className="-ml-1" />
         <div className="ml-auto flex items-center gap-4">
           <ModeToggle />
-          {stats?.outOfStock ? (
-            <span className="relative flex size-4">
-              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 dark:bg-red-400"></span>
-              <span className="relative inline-flex size-4 rounded-full animate-pulse bg-red-400 dark:bg-red-400"></span>
-            </span>
-          ) : stats?.lowStock ? (
-            <span className="relative flex size-4">
-              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-yellow-400 dark:bg-yellow-400"></span>
-              <span className="relative inline-flex size-4 rounded-full animate-pulse bg-yellow-400 dark:bg-yellow-400"></span>
-            </span>
-          ) : (
-            <span className="relative flex size-4">
-              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 dark:bg-green-400"></span>
-              <span className="relative inline-flex size-4 rounded-full animate-pulse bg-green-400 dark:bg-green-400"></span>
-            </span>
-          )}
+          <Link href="/admin/dashboard">
+            {stats?.outOfStock ? (
+              <span className="relative flex size-4">
+                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 dark:bg-red-400"></span>
+                <span className="relative inline-flex size-4 rounded-full animate-pulse bg-red-400 dark:bg-red-400"></span>
+              </span>
+            ) : stats?.lowStock ? (
+              <span className="relative flex size-4">
+                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-yellow-400 dark:bg-yellow-400"></span>
+                <span className="relative inline-flex size-4 rounded-full animate-pulse bg-yellow-400 dark:bg-yellow-400"></span>
+              </span>
+            ) : (
+              <span className="relative flex size-4">
+                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 dark:bg-green-400"></span>
+                <span className="relative inline-flex size-4 rounded-full animate-pulse bg-green-400 dark:bg-green-400"></span>
+              </span>
+            )}
+          </Link>
         </div>
       </div>
     </header>
